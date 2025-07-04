fe458588abfead12f940b1bab895fe02a9665398
Daniel Monroy <rldmonroy@gmail.com>
2025-07-04 09:57:41 -0700

    feat(danielm): server set up

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true }
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  };
+  
